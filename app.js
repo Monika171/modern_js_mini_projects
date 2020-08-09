0313c269efb8d10405700cd30bdb621b9a1837ed
Monika171 <monikarabha171@gmail.com>
2020-08-10 04:03:24 +0900

    adding test images

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
-//LESSON 29
+//LESSON 30
 
+// //LESSON 29
 
+// // document.querySelector('.clear-tasks').addEventListener('click',
+// //   function(e){
+// //     console.log('Hello World');
 
+// //     //e.preventDefault();
+// //   });
+
+// // document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+// // function onClick(e){
+// //   console.log('Clicked');
+// //   //e.preventDefault();
+// // }
+
+// document.querySelector('.clear-tasks').addEventListener('click', onClick);
+
+// //document.querySelector('.clear-tasks').addEventListener('mouseover', onClick);
+
+// function onClick(e){
+//   //console.log('Clicked');
+//   let val;
+
+//   val = e;
+// // event target element
+
+// val = e.target;
+// val = e.target.id;
+// val = e.target.className;
+// val = e.target.classList;
+// e.target.innerText = 'Hello';
+
+// // event type
+// val =e.type;
+
+// //timestamp
+
+// val = e.timeStamp;
+
+// //coords event relative to the window;
+// val = e.clientY;
+// //val = e.clientX;
+// //val = e.offsetY;
+// val = e.offsetX;
+//   console.log(val);
+// }
 
 // //LESSON 28
 // //REPLACE ELEMENT
