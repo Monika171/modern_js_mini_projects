f9272cb194413ec27292d88987feed4b87dbdf94
Monika171 <monikarabha171@gmail.com>
2020-08-22 02:43:39 +0900

    mouse, keyboard, input events

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-//LESSON 30
+//LESSON 31
+
+// //LESSON 30
+// //console.log('aaaaaa');
+// const clearBtn = document.querySelector('.clear-tasks');
+// const card = document.querySelector('.card');
+// const heading = document.querySelector('h5');
+
+// //click
+// // clearBtn.addEventListener('click', runEvent);
+// //double click
+// // clearBtn.addEventListener('dblclick', runEvent);
+// // clearBtn.addEventListener('mousedown', runEvent);
+// // clearBtn.addEventListener('mouseup', runEvent);
+// // card.addEventListener('mouseenter', runEvent);
+// // card.addEventListener('mouseleave', runEvent);
+// // card.addEventListener('mouseover', runEvent);
+// // card.addEventListener('mouseout', runEvent);
+// card.addEventListener('mousemove', runEvent);
+
+
+
+// //event handler
+
+// function runEvent(e){
+//   console.log(`EVENT TYPE: ${e.type}`);
+//   heading.innerHTML = `Mouse: ${e.offsetX} MouseY: ${e.offsetY}`;
+//   document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},40)`;
+// }
 
 // //LESSON 29
 
