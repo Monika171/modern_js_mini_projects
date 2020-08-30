fccb91e1e398601241c782ba7fc6b46dc682bede
Monika171 <monikarabha171@gmail.com>
2020-08-31 03:36:27 +0900

    Dummy Loan Calculator

--- Dummy_Loan_Calculator/app.js
+++ Dummy_Loan_Calculator/app.js
@@ -1,6 +1,15 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  //Hide results
+  document.getElementById('results').style.display = 'none';
 
-function calculateResults(e) {
+  //Show Loader
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+function calculateResults() {
   console.log('Calculating...');
 
   //UI Vars
@@ -23,15 +32,28 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    //Show Results
+    document.getElementById('results').style.display = 'block';
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+
   }else {
     showError('Please check your numbers');
   }
 
-  e.preventDefault();
+  // e.preventDefault();
 }
 
 //Show Error
 function showError(error) {
+
+   //Hide Results
+   document.getElementById('results').style.display = 'none';
+   //Hide loader
+   document.getElementById('loading').style.display = 'none';
+
   //Create a div
   const errorDiv = document.createElement('div');
 
@@ -54,3 +76,63 @@ function showError(error) {
 function clearError(){
   document.querySelector('.alert').remove();
 }
+
+
+// Old Way - No loader Image
+
+// document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// function calculateResults(e) {
+//   console.log('Calculating...');
+
+//   //UI Vars
+//   const amount = document.getElementById('amount');
+//   const interest = document.getElementById('interest');
+//   const years = document.getElementById('years');
+//   const monthlyPayment = document.getElementById('monthly-payment');
+//   const totalPayment = document.getElementById('total-payment');
+//   const totalInterest = document.getElementById('total-interest');
+
+//   const principal = parseFloat(amount.value);
+//   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+//   const calculatedPayments = parseFloat(years.value) * 12;
+
+//   // Compute monthly payment
+//   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+//   const monthly = (principal*x*calculatedInterest)/(x-1);
+
+//   if(isFinite(monthly)){
+//     monthlyPayment.value = monthly.toFixed(2);
+//     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+//     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+//   }else {
+//     showError('Please check your numbers');
+//   }
+
+//   e.preventDefault();
+// }
+
+// //Show Error
+// function showError(error) {
+//   //Create a div
+//   const errorDiv = document.createElement('div');
+
+//   //Get elements
+//   const card = document.querySelector('.card');
+//   const heading = document.querySelector('.heading');
+
+//   errorDiv.className = 'alert alert-danger';
+
+//   errorDiv.appendChild(document.createTextNode(error));
+
+//   //Insert error above heading
+//   card.insertBefore(errorDiv, heading);
+
+//   //Clear error after 3 seconds
+//   setTimeout(clearError, 3000);
+// }
+
+
+// function clearError(){
+//   document.querySelector('.alert').remove();
+// }
