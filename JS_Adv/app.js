518e4acdafc5bcc4dd2f3dd04aaa249df94d6600
Monika171 <monikarabha171@gmail.com>
2020-11-26 11:31:18 +0900

    Symbols

--- JS_Adv/app.js
+++ JS_Adv/app.js
@@ -1,3 +1,48 @@
+// Lesson 90 Symbols
+
+// Create a symbol
+// const sym1 = Symbol();
+// const sym2 = Symbol('sym2');
+
+// console.log(sym2);
+// console.log(typeof sym2); // Primitive data value
+
+// NO two symbols can be the same
+// console.log(Symbol() === Symbol()); //false
+// console.log(Symbol('123') === Symbol('123')); //false
+
+// console.log(`Hello ${sym1}`); // cannot convert Symbol value to a string
+
+// console.log(`Hello ${String(sym1)}`);
+// console.log(`Hello ${sym1.toString()}`);
+
+// Unique Object Keys
+// const KEY1 = Symbol();
+// const KEY2 = Symbol('sym2');
+
+// const myObj = {};
+
+// myObj[KEY1] = 'Prop1';
+// myObj[KEY2] = 'Prop2';
+// myObj.key3 = 'Prop3';
+// myObj.key4 = 'Prop4';
+
+// // console.log(myObj[KEY1]);
+// // console.log(myObj[KEY2]);
+
+// // Symbols are not enumerable in for...in
+// // for(let i in myObj) {
+// //   console.log(`${i}: ${myObj[i]}`);
+// //   // we only get key3 and key4
+// // }
+
+// // Symbols are ignored by JSON.stringify
+// console.log(JSON.stringify({key: 'prop'}));
+// // logs as a json string
+
+// console.log(JSON.stringify({[Symbol('sym1')]: 'prop'}));
+// // empty object {}
+
 // Lesson 88 Iterators and Generators
 
 ////////// Generator Example
